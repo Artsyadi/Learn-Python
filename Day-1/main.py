b7bc712e621afff654b69c7aaf7cf60855c2caf9
Aditya Dawale <dawaleaditya08@gmail.com>
2024-08-19 13:56:29 +0530

    add Day-1 of Python learning

---
+++ Day-1/main.py
@@ -0,0 +1,10 @@
+# print("Welcome to the Band Name Generator.")
+# street = input("What's the name of the city you grew up in?\n")
+# pet = input("What's your pet's name?\n")
+# print("Your band name could be " + street + " " + pet)
+
+print("Welcome to the Band name generator")
+city = input("City you grew up in?\n")
+pet = input("Name of your pet?\n")
+print("City you grew up was " + city + " Name of pet is " + pet)
+
